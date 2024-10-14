b8489e97fe1e50261cfb5f8c44a698500b25ffa8
Jackson Addy <100393568+JaxxonX3@users.noreply.github.com>
2024-10-14 01:35:15 +0000

    Still work in progress I think

--- Main.java
+++ Main.java
@@ -1,48 +1,54 @@
 /********************************************
-*	AUTHORS:	<your names>
-* COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
+*	AUTHORS:	Jackson Addy
+*	LAST MODIFIED:	10/13/2024
 ********************************************/
 
 /********************************************
-*	<TITLE OF PROGRAM>
-*********************************************
+Item Percentage Caculator 
 *	PROGRAM DESCRIPTION:
-*	<1-2 sentences describing overall program>
+*The purpose of my program is to provide a convenient way to easily calcualte the percentage discount on any item that may be purchased.
+*Often, I come across deals, like 15% off, and I have no idea what the final cost would be.
+*I realized that through computer science, I could address this weakness by having a computer quickly cacluate the total for me. 
+/********************************************
+
 *********************************************
 *	ALGORITHM:
-*	<Pseudocode here>
+*	Get the users input for the discount percentage and the items price. 
+*Caculate the amount saved based on the discount entered. 
+*Caculate then display the final price with the discount.
 *********************************************
+
+/********************************************
 *	STATIC METHODS:
-* <list of static methods and which teammate made each>
+* I worked alone
 *********************************************/
 
 
 public class Main 
 {
   /***** CONSTANT SECTION *****/
-
+  
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
-   double discount = 47;
-   double itemPrice = 50;
-   double result = discountPrice(discount,itemPrice);
-  
+    double discount, itemPrice;
+    
     /***** INITIALIZATION SECTION *****/
-
-    /***** INTRO SECTION *****/
-
+    discount = UtilityBelt.readDouble("Discount %: ", 1, 9999);  // Read discount percentage
+    itemPrice = UtilityBelt.readDouble("Item's Price: ", 1, 9999);  // Read item price
+    
     /***** PROCESSING SECTION *****/
-
+    double result = discountPrice(discount, itemPrice);  // Calculate the discount amount
+  
     /***** OUTPUT SECTION *****/
-    System.out.println("if your item cost $" + itemPrice + " and you have a discount of %" + discount + " you will be saving $" + result + "making your overall price $" + (itemPrice - result));
+    System.out.printf("If your item costs $%5.2f and you have a discount of %.2f%%, you will be saving $%5.2f, making your overall price $%5.2f.\n",  itemPrice, discount, result, (itemPrice - result));
   }
+
   /***** STATIC METHODS *****/
-  public static double discountPrice(double num1, double num2)
+  public static double discountPrice(double discount, double price)
   {
-    double numDisc = num1 / 100;
-    double priceOff = num2 * numDisc;
+    double discountRate = discount / 100;
+    double priceOff = price * discountRate;
     return priceOff;
   }
 }
